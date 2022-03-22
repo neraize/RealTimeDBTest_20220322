2d29ebe4b046c91a2d72eb7b935d203257787d47
seojungmin <neraize@gmail.com>
2022-03-22 10:52:58 +0900

    db변경시 => 마지막으로 추가된 사항을 , ChattingData로 변환/목록에 추가 -변경 항목 전체 중, 원하는 항목을 찾아가서 활용

--- app/src/main/java/com/neraize/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/neraize/realtimedbtest_20220322/MainActivity.kt
@@ -6,13 +6,17 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.neraize.realtimedbtest_20220322.datas.ChattingData
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : BaseActivity() {
 
     var messageCount =0L // db에 저장된 채팅 갯수를 담을 변수. Long 타입으로 저장
+
+    val mChattingList:ArrayList<ChattingData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,14 @@ class MainActivity : BaseActivity() {
 
                 // snapshot 변수 : 현재 변경된 상태 => 자녀가 몇개인지 추출
                 messageCount =  snapshot.childrenCount
+
+                // snapshot => 마지막 자녀 추출 => ChattingDat로 변환 + 목록에 추가
+                mChattingList.add(
+                    ChattingData(
+                        snapshot.children.last().child("content").value.toString(),
+                        snapshot.children.last().child("createdAt").value.toString()
+                    )
+                )
             }
 
             override fun onCancelled(error: DatabaseError) {
