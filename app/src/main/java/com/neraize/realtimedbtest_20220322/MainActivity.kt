ecfe99b7f91d8052629d1bcf25d0e2794c0ddcda
seojungmin <neraize@gmail.com>
2022-03-22 10:18:04 +0900

    realtimeDb에, 하위항목구조 설정 -데이터 기록 체험

--- app/src/main/java/com/neraize/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/neraize/realtimedbtest_20220322/MainActivity.kt
@@ -3,6 +3,9 @@ package com.neraize.realtimedbtest_20220322
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,20 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        btnSend.setOnClickListener {
+            val inputContent = edtContent.text.toString()
+
+            // 임시 : DB의 하위항목으로 => message 항목생성 => 그밑에 0번항목의 => content 항목에 입력내용을 저장
+            realtimeDB.getReference("message").child("0").child("content").setValue(inputContent)
+
+            // 추가 기록 : 현재 시간값을 "2022년 3월5일 오후 5:05" 양식으로 기록
+            val now = Calendar.getInstance()
+            val sdf = SimpleDateFormat("yyyy년 M월 d일 a h:mm")
+            val nowStr = sdf.format(now.time)
+
+            realtimeDB.getReference("message").child("0").child("createdAt").setValue(nowStr)
+
+        }
     }
 
     override fun setValues() {
